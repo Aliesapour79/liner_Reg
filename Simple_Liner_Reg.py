c6c9aa611a86124f3218a33ab077a9eb717ee045
unknown <aliesapour79@gmail.com>
2024-09-05 00:32:26 +0330

    Change Comments

--- Simple_Liner_Reg.py
+++ Simple_Liner_Reg.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt 
 import numpy as np 
+# Data points
 
 xpoint = np.array([1,2,3,4])
 ypoint = np.array([2,3,5,4])
 
-#b+mx=y
-##2+0.5x=y
+# Linear regression equation: y = mx + b
 
 def square(x, result=[]):
     for y in x:
@@ -29,16 +29,24 @@ def liner_reg(xp, yp):
     return m, b
 
 m, b = liner_reg(xpoint, ypoint)
+
+# Predict y values based on the regression model
+
 y_p = []
 for x in xpoint:
     y = b + m * x
     y_p.append(y)
 
+
+# Calculate errors and metrics
+
 error = ypoint - y_p
 error_sq = error**2
 sum_square = sum(error_sq)
-
 mse = sum_square / len(xpoint)
+
+# Display results
+
 
 print("Predicted y: ", y_p)
 print("Error: ", error)
@@ -46,7 +54,10 @@ print("E^2: ", error_sq)
 print("Sum of Squared Error: ", sum_square)
 print("MSE: ", mse)
 
-# Plotting the points and the regression line
+
+# Plot the data and the regression line
+
+
 plt.scatter(xpoint, ypoint, color="red", label="Original Data")
 plt.plot(xpoint, y_p, color="blue", label="Fitted Line")
 plt.xlabel("X")
